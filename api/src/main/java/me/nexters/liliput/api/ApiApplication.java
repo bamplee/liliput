cb8e52cf84e1129b379f3fb68099bf4fe2b38214
bamplee <bamplee@gmail.com>
2018-04-04 00:10:49 +0900

    Add spring security using JWT

--- api/src/main/java/me/nexters/liliput/api/ApiApplication.java
+++ api/src/main/java/me/nexters/liliput/api/ApiApplication.java
@@ -4,6 +4,7 @@ import me.nexters.liliput.api.common.util.ShortUrlBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @SpringBootApplication
 public class ApiApplication {
