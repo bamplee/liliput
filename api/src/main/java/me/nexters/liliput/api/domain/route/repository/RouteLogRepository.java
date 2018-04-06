44157d063b3ee83e75b50bde38b59f217dc54f26
bamplee <bamplee@gmail.com>
2018-04-06 23:08:43 +0900

    Add route logging on short path request

---
+++ api/src/main/java/me/nexters/liliput/api/domain/route/repository/RouteLogRepository.java
@@ -0,0 +1,6 @@
+package me.nexters.liliput.api.domain.route.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RouteLogRepository extends JpaRepository<RouteLog, Long> {
+}
